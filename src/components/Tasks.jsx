e671b13a8de01017d2407ad79e2c4a61ca743447
Sergio <sergiobrito17020@gmail.com>
2025-03-31 21:12:33 -0300

    style: added color to trash icon and check/uncheck icons

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,9 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import {
+  CheckIcon,
+  ChevronRightIcon,
+  SquareIcon,
+  TrashIcon,
+} from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -18,17 +23,18 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted ? <CheckIcon /> : <SquareIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
             <ChevronRightIcon />
           </Button>
           <Button onClick={() => onDeleteTaskClick(task.id)}>
-            <TrashIcon />
+            <TrashIcon className="text-red-600" />
           </Button>
         </li>
       ))}
